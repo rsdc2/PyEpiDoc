544f325fd441af5f44f05620515ad76a53b68937
Robert Crellin <robert.crellin@gmail.com>
2023-03-27 11:10:27 +0100

    added DocString for ab

--- pyepidoc/epidoc/ab.py
+++ pyepidoc/epidoc/ab.py
@@ -23,7 +23,25 @@ from .constants import XMLNS
 class Ab(Element):
 
     """
-    
+    The Ab class provides services for interaction with 
+    a documents <ab> elements.
+    <ab> stands for 'anonymous block', see 
+    https://epidoc.stoa.org/gl/latest/ref-ab.html:
+
+    "<ab> (anonymous block) contains any arbitrary component-level 
+    unit of text, acting as an anonymous container for 
+    phrase or inter level elements analogous to, 
+    but without the semantic baggage of, a paragraph." 
+    (last accessed 2023-03-27)
+
+    From the perspective of accessing / creating tokens,
+    <ab> is the domain of tokens.
+    The Ab class therefore carries the method for 
+    actually doing the tokenization / collecting the tokens.
+    Equivalent methods in the Edition and EpiDocCorpus
+    classes call these methods for each <ab> contained
+    within them.
+
     """
 
     def __init__(self, e:Optional[_Element]=None):
