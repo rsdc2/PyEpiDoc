7d2ae2794b67e3ca5227e3a4f130bf85a5090790
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-04-13 10:37:39 +0100

    assert len(edition.expans) == 3

--- tests/api/expected/test_epidoc_expected.py
+++ tests/api/expected/test_epidoc_expected.py
@@ -29,7 +29,7 @@ def test_expans():
     edition = head(doc.editions)
 
     assert edition != None
-    assert len(edition.expans) != None
+    assert len(edition.expans) == 3
 
 
 @pytest.mark.parametrize("filepath", relative_filepaths)
