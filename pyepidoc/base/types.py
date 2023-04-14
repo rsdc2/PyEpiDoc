fc2e9c7b4441976661ca343cc9a6258ee840584d
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-04-14 14:12:02 +0100

    added base types

---
+++ pyepidoc/base/types.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+import abc
+from typing import Sequence, TypeVar
+_T = TypeVar('_T') 
+
+
+class Showable:
+    @abc.abstractmethod
+    def __str__(self) -> str:
+        ...
+
+
+class ExtendableSeq(Sequence[_T]):
+    @abc.abstractmethod
+    def __add__(self, other) -> ExtendableSeq[_T]:
+        ...
