ab18ead7ce4452e3e3a0e8ee4254ea2ac84fda90
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-04-17 16:34:15 +0100

    added tests for lang function

--- tests/api/expected/test_epidoc_expected.py
+++ tests/api/expected/test_epidoc_expected.py
@@ -1,15 +1,17 @@
 from pyepidoc.epidoc.epidoc import EpiDoc
 from pyepidoc.utils import head
+from pyepidoc.epidoc.funcs import lang
 
 import pytest
 
-relative_filepaths = [
-    'api/files/single_files_untokenized/ISic000001.xml'
-]
+relative_filepaths = {
+    'ISic000001': 'api/files/single_files_untokenized/ISic000001.xml',
+    'lang': 'api/files/langs.xml'
+}
 
 
 def test_collect_tokens():
-    filepath = relative_filepaths[0]
+    filepath = relative_filepaths['ISic000001']
     doc = EpiDoc(filepath, fullpath=False)
 
     assert doc.tokens_list_str == [
@@ -23,7 +25,7 @@ def test_collect_tokens():
 
 
 def test_expans():
-    filepath = relative_filepaths[0]
+    filepath = relative_filepaths['ISic000001']
     
     doc = EpiDoc(filepath, fullpath=False)
     edition = head(doc.editions)
@@ -32,7 +34,19 @@ def test_expans():
     assert len(edition.expan_elems) == 3
 
 
-@pytest.mark.parametrize("filepath", relative_filepaths)
+def test_langs():
+    """
+    Tests that the collecting of language information happens in the correct way.
+    """
+
+    doc = EpiDoc(relative_filepaths['lang'], fullpath=False)
+
+    assert doc.textlangs == {'la', 'grc'}
+    assert lang(head(doc.expans)) == 'la'
+    assert lang(head(doc.tokens)) == 'grc'
+
+
+@pytest.mark.parametrize("filepath", relative_filepaths.values())
 def test_load_relative_filepath_from_str(filepath:str):
     doc = EpiDoc(filepath, fullpath=False)
     assert doc.tokens_list_str != []
