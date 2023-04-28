b67099600fbf7f3b63ed6f96c5f250aa65d30c04
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-04-28 15:29:33 +0100

    removed duplication NS and XMLNS

--- pyepidoc/constants.py
+++ pyepidoc/constants.py
@@ -17,9 +17,6 @@ WHITESPACE_RE = r'(\s+)'
 
 VERBOSE = False
 
-NS = "http://www.tei-c.org/ns/1.0"
-XMLNS = "http://www.w3.org/XML/1998/namespace"
-
 SubsumableRels = [
     {'head': {'name': 'w', 'ns': NS, 'attrs': dict()}, 'dep': {'name': 'lb', 'ns': NS, 'attrs': {'break': 'no'}}},
     {'head': {'name': 'w', 'ns': NS, 'attrs': dict()}, 'dep': {'name': 'g', 'ns': NS, 'attrs': dict()}},
