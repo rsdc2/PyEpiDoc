8bd1a8df5cd5a5555878410eddd0b9a7046df1eb
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-07-31 15:20:00 +0100

    removed examples

--- example_dates.py
+++
@@ -1,8 +0,0 @@
-from pyepidoc.epidoc.corpus import EpiDocCorpus
-    
-def corpus_dates():
-    corpus = EpiDocCorpus(folderpath='/home/robert/Documents/programming/python/crossreads/pyarethusadoc/PyArethusaDoc/data/ud_linguistic_annotation', fullpath=True).filter_by_form(['vixit', 'ἔζησεν'])
-    print(corpus.datemin)
-    print(corpus.datemax)
-
-corpus_dates()
