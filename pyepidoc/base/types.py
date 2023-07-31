7dac1dd83416377c792afc1984792adf43f85e40
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-07-31 15:56:59 +0100

    moved EnumerableEnum

--- pyepidoc/base/types.py
+++ pyepidoc/base/types.py
@@ -2,7 +2,15 @@ from __future__ import annotations
 from collections import namedtuple
 import abc
 from typing import Sequence, TypeVar
+from enum import Enum
 _T = TypeVar('_T') 
+
+
+class EnumerableEnum(Enum):
+
+    @classmethod
+    def values(cls) -> list:
+        return [item.value for item in cls]
 
 
 class Showable:
