5e019e35f30f56ad2876d3f3b60635f055ff5d3e
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-08-03 11:34:11 +0100

    refactoring, xml model headers

--- tests/tokenize/test_tokenize_intended.py
+++ tests/tokenize/test_tokenize_intended.py
@@ -64,20 +64,10 @@ tests = [
 ]
 
 
-@pytest.mark.parametrize("tokenize_type", tests)
-def test_tokenize(tokenize_type:str):
-    filename = f'{tokenize_type}.xml'
-    untokenized_folderpath = 'tokenize/files/untokenized'
-    tokenized_folderpath = 'tokenize/files/tokenized_output'
-    benchmark_folderpath = 'tokenize/files/tokenized_benchmark'
-    untokenized_filepath = filepath_from_list([untokenized_folderpath], filename)
-    tokenized_filepath = filepath_from_list([tokenized_folderpath], filename)
-    benchmark_filepath = filepath_from_list([benchmark_folderpath], filename)
-
-    # Remove old output files if they exist
+def remove_file(filepath:str):
     try:
         tokenized_f = FileInfo(
-            filepath=tokenized_filepath,
+            filepath=filepath,
             mode = FileMode.r.value,
             fullpath=False
         )
@@ -85,7 +75,35 @@ def test_tokenize(tokenize_type:str):
             os.remove(tokenized_f.full_filepath)
     except FileExistsError:
         pass
-    
+
+
+def get_path_vars(tokenize_type:str) -> tuple[str, str, str, str, str, str]:
+    filename = f'{tokenize_type}.xml'
+    untokenized_folderpath = 'tokenize/files/untokenized'
+    tokenized_folderpath = 'tokenize/files/tokenized_output'
+    benchmark_folderpath = 'tokenize/files/tokenized_benchmark'
+    tokenized_filepath = filepath_from_list([tokenized_folderpath], filename)
+    benchmark_filepath = filepath_from_list([benchmark_folderpath], filename)
+
+    return (filename, 
+            untokenized_folderpath, 
+            tokenized_folderpath, 
+            benchmark_folderpath, 
+            tokenized_filepath, 
+            benchmark_filepath)
+
+
+def tokenize_epidoc(tokenize_type:str) -> tuple[EpiDoc, EpiDoc]:
+    (filename, 
+            untokenized_folderpath, 
+            tokenized_folderpath, 
+            benchmark_folderpath, 
+            tokenized_filepath, 
+            benchmark_filepath) = get_path_vars(tokenize_type)
+
+    # Remove old output file if exists
+    remove_file(tokenized_filepath)
+
     # Tokenize the files
     tokenize(
         src_folderpath=untokenized_folderpath, 
@@ -110,7 +128,23 @@ def test_tokenize(tokenize_type:str):
 
     tokenized_epidoc = EpiDoc(tokenized_f)
     tokenized_benchmark = EpiDoc(benchmark_f)
+
+    return tokenized_epidoc, tokenized_benchmark
+
+
+def test_model_headers():
+    # Tokenize the files
+    tokenized_epidoc, tokenized_benchmark = tokenize_epidoc(tokenize_type='xml_model_headers_1.xml')
+
+
     
+
+@pytest.mark.parametrize("tokenize_type", tests)
+def test_tokenize(tokenize_type:str):
+    # Tokenize the files
+    tokenized_epidoc, tokenized_benchmark = tokenize_epidoc(tokenize_type=tokenize_type)
+
+    # Do the tests    
     assert [str(word) for word in tokenized_epidoc.tokens] == [str(word) for word in tokenized_benchmark.tokens]
     assert [word.xml for word in tokenized_epidoc.tokens] == [word.xml for word in tokenized_benchmark.tokens]
     assert [word.xml for word in tokenized_epidoc.compound_words] == [word.xml for word in tokenized_benchmark.compound_words]
