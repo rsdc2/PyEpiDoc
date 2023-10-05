6da76a7ad092480e7f70c192faee843d525bf3f7
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-10-05 12:53:31 +0100

    added docstr

--- tests/api/unintended/test_corpus_unintended.py
+++ tests/api/unintended/test_corpus_unintended.py
@@ -3,10 +3,19 @@ from pyepidoc import EpiDocCorpus
 import os
 
 def test_load_corpus_local():
+    """
+    Test that raises an error if asked to load a corpus
+    from a non-existent local folder
+    """
     with pytest.raises(FileExistsError):
         _ = EpiDocCorpus('api/files/non_existent_folder', fullpath=False)
 
 
 def test_load_corpus_root():
+    """
+    Test that raises an error if asked to load a corpus
+    from a non-existent folder specified from root
+    """
+
     with pytest.raises(FileExistsError):
         _ = EpiDocCorpus(os.getcwd() + '/api/files/non_existent_folder', fullpath=True)
