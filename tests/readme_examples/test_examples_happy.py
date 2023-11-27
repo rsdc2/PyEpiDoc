fb29644aa734aadc6d087fff2df6d150f04f1918
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-11-27 15:51:24 +0000

    use default parameters on tokenize

--- tests/readme_examples/test_examples_happy.py
+++ tests/readme_examples/test_examples_happy.py
@@ -16,18 +16,11 @@ def test_tokenize_example():
     # Load the EpiDoc file
     doc = EpiDoc("readme_examples/files/input/ISic000032_untokenized.xml")
 
-    # Tokenize the edition
+    # Tokenize the edition with default settings
     doc.tokenize()
-
-    # Prettify the edition
-    doc.prettify_edition()
-
-    # Add spaces between tokens
-    doc.space_tokens()
 
     # Save the results to a new XML file
     doc.to_xml_file("readme_examples/files/tokenized_output/ISic000032_tokenized.xml")
-
 
     tokenized_doc = EpiDoc("readme_examples/files/tokenized_output/ISic000032_tokenized.xml")
     tokenized_benchmark = EpiDoc("readme_examples/files/tokenized_benchmark/ISic000032_tokenized.xml")
