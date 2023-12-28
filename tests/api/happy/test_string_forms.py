d77c86239bcf02b80af840e1ad647e55dd5dbf9c
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-12-28 13:14:33 +0000

    first commit of test_string_forms

---
+++ tests/api/happy/test_string_forms.py
@@ -0,0 +1,28 @@
+"""
+Tests that the string representations of individual tokens,
+or sequences of tokens, are as expected.
+"""
+
+from pyepidoc.epidoc.elements.ab import Ab
+from pyepidoc.xml.utils import elem_from_str, abify
+
+import pytest
+
+tests = [
+    ('<w><expan><abbr><num value="2"><hi rend="intraline">II</hi></num>vir</abbr><ex>o</ex></expan></w>', 
+     ['IIviro']), 
+    ('<w><expan><abbr><num value="2">II</num>vir</abbr><ex>o</ex></expan></w>', 
+     ['IIviro']), 
+]
+
+
+@pytest.mark.parametrize(['xml', 'tokens'], tests)
+def test_string_forms(xml: str, tokens: list[str]):
+    """
+    Tests token strings correct
+    """
+
+    ab = Ab(elem_from_str(abify(xml)))
+    assert ab.tokens_list_str == tokens
+
+
