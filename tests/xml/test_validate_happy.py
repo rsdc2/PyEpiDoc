ededd412b4b94cfb6d3cb3cf0784ecdbefdcbbad
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-05 10:32:23 +0000

    add test that validation doesn't change the file

--- tests/xml/test_validate_happy.py
+++ tests/xml/test_validate_happy.py
@@ -24,3 +24,17 @@ def test_validate_on_load():
 def test_validate_on_load_2():
     _ = EpiDoc('xml/files/ISic000001_with_xinclude.xml', 
                validate_on_load=True)
+    
+
+def test_validate_does_not_change_file():
+    """
+    Tests that applying xinclude in validation doesn't
+    change the EpiDoc file
+    """
+    doc1 = EpiDoc('xml/files/ISic000001_with_xinclude.xml')
+    
+    doc2 = EpiDoc('xml/files/ISic000001_with_xinclude.xml', 
+               validate_on_load=True)
+    
+    assert doc1.__bytes__() == doc2.__bytes__()
+    
