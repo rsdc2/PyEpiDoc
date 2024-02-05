08f3941361b1c5e1ec9a08b252fe63f58ebedbcf
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-05 11:08:35 +0000

    change import path

--- tests/api/happy/test_analysis.py
+++ tests/api/happy/test_analysis.py
@@ -1,4 +1,4 @@
-from pyepidoc.utils.csv_ops import pivot_dict
+from pyepidoc.utils.csv import pivot_dict
 
 def test_pivot_dict():
     dict_dict = {'a': {'x': 1, 'y': 2, 'z': 3}, 'b': {'x': 10, 'y': 20, 'z': 30}}
