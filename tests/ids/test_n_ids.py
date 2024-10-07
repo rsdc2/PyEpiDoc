9f8e90b6d11971dfa2429a5683abe0e643eded4d
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-10-07 16:02:25 +0100

    add assert statement

--- tests/ids/test_n_ids.py
+++ tests/ids/test_n_ids.py
@@ -16,3 +16,5 @@ def test_set_n_ids():
     doc = EpiDoc(input_path / Path('set_n_ids_1.xml'))
     with_n_ids = doc.set_n_ids()
     with_n_ids.to_xml_file(output_path / Path('set_n_ids_1.xml'))
+
+    assert [token.get_attrib('n') for token in with_n_ids.w_tokens] == ['0', '5']
