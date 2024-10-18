626c47e576fd1f008a373f96fa8b42ed4a241117
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-10-18 10:24:35 +0100

    clearer naming of containing folder

--- tests/workflows/add_n_ids_and_lemmatize/paths.py
+++ tests/workflows/add_n_ids_and_lemmatize/paths.py
@@ -3,7 +3,7 @@ Paths to files in this folder
 """
 from pathlib import Path
 
-WD = Path('tests/workflows/add_ids_and_lemmatize')
+WD = Path('tests/workflows/add_n_ids_and_lemmatize')
 BENCHMARK = WD / Path('benchmark')
 INPUT = WD / Path('input')
 OUTPUT = WD / Path('output')
