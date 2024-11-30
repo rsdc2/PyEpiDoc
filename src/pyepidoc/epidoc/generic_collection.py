fc799a34a305d5f3b96f79870dc4e196f113a6a8
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-11-30 17:19:46 +0000

    add more methods for querying

--- src/pyepidoc/epidoc/generic_collection.py
+++ src/pyepidoc/epidoc/generic_collection.py
@@ -10,7 +10,8 @@ from typing import (
     Generic,
     SupportsIndex,
     overload,
-    TypeVar
+    TypeVar,
+    Hashable
 )
 
 from pyepidoc.shared.utils import top, contains, listfilter
@@ -44,6 +45,26 @@ class GenericCollection(Generic[T]):
         Alias for length property
         """
         return self.length
+    
+    def foreach(self, action: Callable[[T], None]) -> None:
+        """
+        Call an action on each element
+        """
+
+        for item in self._values:
+            action(item)
+    
+    def frequencies(self) -> GenericCollection[tuple[T, int]]:
+        """
+        Return a dictionary with the frequencies of each item
+        """
+
+        d = {k: self._values.count(k) 
+                for k in self.unique()._values}
+
+        l = ([(value, frequency) for value, frequency in d.items()])
+        sorted_l = sorted(l, key=lambda item: item[1], reverse=True)
+        return GenericCollection(sorted_l)
 
     @property
     def length(self) -> int:
@@ -58,6 +79,28 @@ class GenericCollection(Generic[T]):
         Map a function to the abbreviations
         """
         return GenericCollection(list(map(func, self._values)))
+    
+    def sort(self, key=lambda x: x, reverse: bool=False) -> GenericCollection[T]:
+        """
+        Sort the values according to a key function
+        """
+        sorted_values = sorted(self._values, key=key, reverse=reverse)
+        return GenericCollection(sorted_values)
+
+    def to_list(self) -> list[T]:
+        """
+        Return the underlying _values `list` object
+        """
+        return self._values
+
+    def top(self, n: int) -> GenericCollection[T]:
+        """
+        Take the top `n` items
+        """
+        if self.count > n:
+            return GenericCollection(self._values[0:n])
+        
+        return GenericCollection(self._values)
     
     def unique(self) -> GenericCollection[T]:
         """
