b1ce6243931d2f01421ebef994dfcc85b9f1c1c3
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-01-23 06:53:03 +0000

    Split out prettifying tests

--- tests/api/happy/test_epidoc_happy.py
+++ tests/api/happy/test_epidoc_happy.py
@@ -197,55 +197,7 @@ def test_load_relative_filepath_from_str(filepath: str):
 def test_materialclasses():
     doc = EpiDoc(relative_filepaths['ISic000001'])
     assert doc.materialclasses == ['#material.stone.marble']
-
-    # 'benchmark_lxml': 'tests/api/files/prettifying/benchmark/ISic000552_prettified_lxml.xml',
-    # 'benchmark_pyepidoc': 'tests/api/files/prettifying/benchmark/ISic000552_prettified_pyepidoc.xml',
-    # 'prettified_lxml': 'tests/api/files/prettifying/prettified/ISic000552_prettified_lxml.xml',
-    # 'prettified_pyepidoc': 'tests/api/files/prettifying/prettified/ISic000552_prettified_pyepidoc.xml',
-
-# def test_prettify_doc_with_lxml():
-#     """
-#     Tests that the entire document is prettified correctly
-#     using lxml's inbuilt prettifier.
-#     Prettifies both the main document and the editions.
-#     """
-
-#     ugly = EpiDoc(relative_filepaths['ugly'])
-#     prettified = ugly.prettify('lxml')
-#     prettified.to_xml_file(relative_filepaths['prettified_lxml'])
-#     prettified_str = etree.tostring(prettified._e)
-
-#     benchmark = EpiDoc(relative_filepaths['benchmark_lxml'])
-#     benchmark_str = etree.tostring(benchmark._e)
-
-#     assert prettified_str == benchmark_str
-
-
-prettify_pyepidoc_paths = [
-    ('tests/api/files/prettifying/ugly/ISic000552.xml',
-        'tests/api/files/prettifying/prettified/ISic000552_prettified_pyepidoc.xml',
-        'tests/api/files/prettifying/benchmark/ISic000552_prettified_pyepidoc.xml'
-        )
-]
-@pytest.mark.parametrize(("ugly", "prettified", "benchmark"), prettify_pyepidoc_paths)
-def test_prettify_doc_with_pyepidoc(ugly: str, prettified: str, benchmark: str):
-
-    """
-    Tests that the entire document is prettified correctly
-    using pyepidoc's prettifier.
-    Prettifies both the main document and the editions.
-    """
-
-    ugly_doc = EpiDoc(ugly)
-    prettified_doc = ugly_doc.prettify('pyepidoc')
-
-    assert save_reload_and_compare_with_benchmark(
-        doc=prettified_doc,
-        target_path=prettified,
-        benchmark_path=benchmark,
-        output_write_mode=FILE_WRITE_MODE
-    )
-
+    
 
 def test_punct():
     """
