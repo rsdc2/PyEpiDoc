6fd93402ad0338397b02e553a2968554d01c0c90
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-03-19 13:25:50 +0000

    Change file path to posix

--- tests/epidoc/test_corpus.py
+++ tests/epidoc/test_corpus.py
@@ -2,7 +2,7 @@ from pyepidoc import EpiDocCorpus
 
 def test_corpus_lemmatizable():
     # Arrange
-    corpus = EpiDocCorpus(r'tests\api\files\corpus')
+    corpus = EpiDocCorpus(r'tests/api/files/corpus')
 
     # Act
     lemmatizable_count = corpus.lemmatizable_files().count
