1d9cfac8ebeb1c52e82adcecdf525bf8cc84e64a
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-05-01 17:53:07 +0100

    Inherit W in Measure

--- src/pyepidoc/epidoc/elements/measure.py
+++ src/pyepidoc/epidoc/elements/measure.py
@@ -1,2 +1,14 @@
-class Measure:
-    pass
+from lxml.etree import _Element
+
+from .w import W
+
+class Measure(W):
+
+    def __init__(self, e: _Element):
+        if not isinstance(e, _Element):
+            raise TypeError('e should be an instance of type _Element.')
+
+        self._e = e
+
+        if self.localname != 'measure':
+            raise TypeError('Element should be <measure>.')
