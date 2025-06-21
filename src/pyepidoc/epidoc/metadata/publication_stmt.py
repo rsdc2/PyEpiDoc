98a587669264b077cff5838f52850d4ec145b15a
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-06-21 14:18:08 +0100

    Implement idnos property

--- src/pyepidoc/epidoc/metadata/publication_stmt.py
+++ src/pyepidoc/epidoc/metadata/publication_stmt.py
@@ -1,23 +1,16 @@
 from __future__ import annotations
-from typing import overload
-
-from lxml import etree
-from lxml.etree import _Element
-
 from pyepidoc.shared.constants import TEINS, XMLNS
-from pyepidoc.shared.utils import head
-from pyepidoc.xml.namespace import Namespace as ns
 from pyepidoc.epidoc.epidoc_element import EpiDocElement
-from pyepidoc.xml.xml_element import XmlElement
 from .idno import Idno
 
 class PublicationStmt(EpiDocElement):
     """
-    The <respStmt> node, including collections of
-    <respStmt>
+    The <publicationStmt> node, including collections of
+    <publicationStmt>
     """
 
     @property
     def idnos(self) -> list[Idno]:
 
-        return self.get_child_tokens()
+        elements = self.child_elements_by_local_name('idno')
+        return list(map(Idno, elements))
