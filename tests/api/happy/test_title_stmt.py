84e5b62c350fd83b118ae401aec0b858f0b7bcd1
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2025-08-12 21:30:49 +0100

    Only add <respStmt> if does not already exist

--- tests/api/happy/test_title_stmt.py
+++ tests/api/happy/test_title_stmt.py
@@ -1,11 +1,12 @@
 from pyepidoc import EpiDoc
 from pyepidoc.epidoc.metadata.title_stmt import TitleStmt
 from .test_epidoc_happy import relative_filepaths
+from tests.config import EMPTY_TEMPLATE_PATH
 
 def test_append_resp_stmt():
 
     # Arrange
-    epidoc = EpiDoc(relative_filepaths['ISic000001'])
+    epidoc = EpiDoc(EMPTY_TEMPLATE_PATH)
     title_stmt = epidoc.title_stmt
     resp_stmt_count = len(title_stmt.resp_stmts)
 
@@ -15,3 +16,19 @@ def test_append_resp_stmt():
     
     # Assert
     assert new_resp_stmt_count == resp_stmt_count + 1
+
+
+def test_does_not_append_resp_stmt_if_already_exists():
+
+    # Arrange
+    epidoc = EpiDoc(EMPTY_TEMPLATE_PATH)
+    title_stmt = epidoc.title_stmt
+    title_stmt.append_new_resp_stmt("Robert Crellin", "RC", "xyz", "abc")
+    resp_stmt_count = len(title_stmt.resp_stmts)
+
+    # Act
+    title_stmt.append_new_resp_stmt("Robert Crellin", "RC", "xyz", "abc")
+    new_resp_stmt_count = len(title_stmt.resp_stmts)
+
+    # Assert
+    assert new_resp_stmt_count == resp_stmt_count
