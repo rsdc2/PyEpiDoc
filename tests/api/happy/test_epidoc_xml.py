daf31345ee0242458dc5f10ee706239a5760ae71
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2026-02-16 21:31:22 +0000

    Fix test

--- tests/api/happy/test_epidoc_xml.py
+++ tests/api/happy/test_epidoc_xml.py
@@ -1,14 +1,15 @@
-from pyepidoc.epidoc.utils import descendant_text
+from pyepidoc.xml.utils import descendant_text
 from pyepidoc.xml import XmlElement
-from lxml import etree
 
 
 def test_get_descendant_text():
     # from ISic000481
-    xmlstr = "<expan><abbr>A<am>A</am>u</abbr></expan>"
-
+    # Arrange
+    xmlstr = '<expan><abbr>A<am>A</am>u</abbr></expan>'
     elem = XmlElement.from_str(xmlstr)
 
-    assert descendant_text(elem) == "AAu"
+    # Act
+    desc_text = descendant_text(elem)
 
-
+    # Assert
+    assert desc_text == 'AAu'
